c49c46d815c0a3911aaa01a2ea87960a20064daa
AitorMP <aitor.martinez.pons@gmail.com>
2022-06-25 12:06:19 +0100

    update singleGamepage

--- client/src/pages/SingleGamePage/SingleGamePage.jsx
+++ client/src/pages/SingleGamePage/SingleGamePage.jsx
@@ -1,26 +1,7 @@
-// import React, { useState } from 'react';
+// import React, { useState } from 'react'
 
 // const SingleGamePage = () => {
-//   const { gameInfo } = props;
 
-<<<<<<< HEAD
-//   const [games, setGames] = useState();
-
-//   return <div></div>;
-=======
-<<<<<<< HEAD
-// const SingleGamePage = () => {
-
-//   const {gameInfo} = props;
-
-//   const [games, setGames] = useState()
-//   let {id} =useState()
-  
-//   return (
-    
-//   <div>
-//     <h1>SingleGamePage</h1>
-=======
 //   const {gameInfo} = props;
 
 //   const [games, setGames] = useState()
@@ -29,11 +10,9 @@
 //   return (
 //   <div>
 //     <h1>SingleGamePage<h1>
->>>>>>> 7bc2b5b46d92f7a3747dae5bc561c706f485c5bf
 //     </div>
 
 // )
->>>>>>> 9ddb3250f7b8c3463f529cac59c0df2d897542da
 // };
 
 // export default SingleGamePage;
