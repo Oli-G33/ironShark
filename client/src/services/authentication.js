b94dd6f2c497fa593548add115aa97128daf4715
OliGarc333 <ngarcia333@gmail.com>
2022-06-25 16:57:08 +0200

    Authentication Completed

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -4,7 +4,7 @@ export const signOutUser = () =>
   api.post('/authentication/sign-out').then(response => response.data);
 
 export const registerUser = body =>
-  api.post('/sign-up', body).then(response => response.data);
+  api.post('/authentication/sign-up', body).then(response => response.data);
 
 export const logInUser = body =>
   api.post('/authentication/sign-in', body).then(response => response.data);
