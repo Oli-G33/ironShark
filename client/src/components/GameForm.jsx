a15d475baddf9295ef96de5e7be84f51b161a0f2
AitorMP <aitor.martinez.pons@gmail.com>
2022-06-28 22:43:13 +0100

    game.js services, addGamePage done, started GameForm

---
+++ client/src/components/GameForm.jsx
@@ -0,0 +1,42 @@
+const GameForm = ({ game, onGameChange, onGameSubmit, buttonLabel }) => {
+  const handleGameFormSubmission = (event) => {
+    event.preventDefault();
+    onGameSubmit();
+  };
+
+  return (
+    <form onSubmit={handleGameFormSubmission}>
+      {/* title: '', ✅
+      gameUrl: '', ✅
+      description: '', 
+      genre: '', ✅
+      price: 'num', 
+      inStock:'true', 
+      fileSize: 'mb', 
+      owner: '', 
+      cover: '', 
+      screenshots: '', 
+      trailer: '' */}
+
+      <label htmlFor="input-title">Game Title</label>
+      <label htmlFor="input-gameUrl">Game's Url</label>
+      <label htmlFor="input-genre">Select Game Genre</label>
+      <select
+        id="input-genre"
+        value={game.genre}
+        onChange={(event) =>
+          onGameChange({ ...game, genre: event.target.value })
+        }
+      >
+        <option value="action">Action</option>
+        <option value="horror ">Horror</option>
+        <option value="adventure">Adventure</option>
+        <option value="racing">Racing</option>
+        <option value="fighting ">Fighting</option>
+        <option value="sports">Sports</option>
+      </select>
+    </form>
+  );
+};
+
+export default GameForm;
--- client/src/services/games.js
+++ client/src/services/games.js
@@ -1,7 +1,24 @@
-// import axios from 'axios';
+import api from './api';
 
-// const gamesApi = axios.create({
+export const gameSearch = (filters) =>
+  api
+    .get(`/game/search?${new URLSearchParams(filters).toString()}`)
+    .then((response) => response.data);
 
-// })
+export const gameLoad = (id) =>
+  api.get(`/game/${id}`).then((response) => response.data);
 
-// export default twitchApi;
+export const gameEdit = (id, game) =>
+  api.patch(`/game/${id}`, game).then((response) => response.data);
+
+export const gameAdd = (game) =>
+  api.post('/game', game).then((response) => response.data);
+
+export const bookmarkList = () =>
+  api.get('/game/bookmarked').then((response) => response.data);
+
+export const bookmarkAdd = (id) =>
+  api.post(`/game/${id}/bookmark`).then((response) => response.data);
+
+export const bookmarkRemove = (id) =>
+  api.delete(`/game/${id}/bookmark`).then((response) => response.data);
