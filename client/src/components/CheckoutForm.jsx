d9a2f0167dbe2be740667fe85404d23c97c097a5
OliGarc333 <ngarcia333@gmail.com>
2022-07-04 18:37:00 +0200

    testing success page netlify

--- client/src/components/CheckoutForm.jsx
+++ client/src/components/CheckoutForm.jsx
@@ -59,7 +59,7 @@ export default function CheckoutForm(props) {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: 'http://localhost:3000/success'
+        return_url: 'https://iron-shark.netlify.app/success'
       }
     });
 
